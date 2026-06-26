761ede32457d305e4f8ea80db82b9430a3e7d14c
Jared Norman <jared@super.gd>
2026-06-25 22:50:18 -0700

    Parse numbers

--- src/root.zig
+++ src/root.zig
@@ -13,6 +13,18 @@ const Stack = struct {
         self.count -= 1;
         return self.storage[self.count];
     }
+
+    fn interpret(self: *Stack, source: []const u8) !void {
+        var words = std.mem.tokenizeAny(u8, source, " \t\r\n");
+
+        while (words.next()) |word| {
+            if (std.fmt.parseInt(i64, word, 10)) |number| {
+                self.push(number);
+            } else |_| {
+                return error.UnknownWord;
+            }
+        }
+    }
 };
 
 test "stack push and pop" {
@@ -23,4 +35,15 @@ test "stack push and pop" {
 
     try std.testing.expectEqual(43, stack.pop());
     try std.testing.expectEqual(42, stack.pop());
+}
+
+test "interpet pushes numbers in order" {
+    var stack = Stack{};
+
+    try stack.interpret("1 10 2 4");
+
+    try std.testing.expectEqual(4, stack.pop());
+    try std.testing.expectEqual(2, stack.pop());
+    try std.testing.expectEqual(10, stack.pop());
+    try std.testing.expectEqual(1, stack.pop());
 }
