a5e5c20cf8b337939d0175c00e31af0b96403ff6
Jared Norman <jared@super.gd>
2026-06-28 11:36:05 -0700

    Use protection

--- src/root.zig
+++ src/root.zig
@@ -4,12 +4,20 @@ const Stack = struct {
     storage: [16]i64 = undefined,
     count: usize = 0,
 
-    fn push(self: *Stack, value: i64) void {
+    fn push(self: *Stack, value: i64) !void {
+        if (self.count >= self.storage.len) {
+            return error.StackOverflow;
+        }
+
         self.storage[self.count] = value;
         self.count += 1;
     }
 
-    fn pop(self: *Stack) i64 {
+    fn pop(self: *Stack) !i64 {
+        if (self.count == 0) {
+            return error.StackUnderflow;
+        }
+
         self.count -= 1;
         return self.storage[self.count];
     }
@@ -19,7 +27,7 @@ const Stack = struct {
 
         while (words.next()) |word| {
             if (std.fmt.parseInt(i64, word, 10)) |number| {
-                self.push(number);
+                try self.push(number);
             } else |_| {
                 return error.UnknownWord;
             }
@@ -30,11 +38,27 @@ const Stack = struct {
 test "stack push and pop" {
     var stack = Stack{};
 
-    stack.push(42);
-    stack.push(43);
+    try stack.push(42);
+    try stack.push(43);
 
     try std.testing.expectEqual(43, stack.pop());
     try std.testing.expectEqual(42, stack.pop());
+}
+
+test "stack underflow" {
+    var stack = Stack{};
+
+    try std.testing.expectError(error.StackUnderflow, stack.pop());
+}
+
+test "stack overflow" {
+    var stack = Stack{};
+
+    for (0..16) |_| {
+        try stack.push(0);
+    }
+
+    try std.testing.expectError(error.StackOverflow, stack.push(0));
 }
 
 test "interpet pushes numbers in order" {
